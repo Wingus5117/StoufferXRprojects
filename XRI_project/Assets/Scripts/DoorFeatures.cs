d56ff3fab30ab6d5c29df43c835f27ea2c4910bb
Wingus5117 <118506016+Wingus5117@users.noreply.github.com>
2024-10-01 10:49:33 -0400

    a

--- XRI_project/Assets/Scripts/DoorFeatures.cs
+++ XRI_project/Assets/Scripts/DoorFeatures.cs
@@ -22,12 +22,49 @@ public class DoorFeatures : CoreFeatures
         SocketInteractor?.selectEntered.AddListener((s) =>
         {
             OpenDoor();
+            PlayOnStart();
         });
+
+        SimpleInteractable?.selectEntered.AddListener((s) =>
+        {
+            OpenDoor();
+        });
+        
     }
 
-    private void OpenDoor()
-    { 
-    
+    public void OpenDoor()
+    {
+        if (!DoorOpen)
+        { 
+            PlayOnStart();
+            DoorOpen = true;
+            StartCoroutine(ProcessMotion());
+        }
     }
-   
+
+    private IEnumerator ProcessMotion()
+    {
+        while (DoorOpen)
+        {
+            var angle = doorPivot.localEulerAngles.y < 100 ? doorPivot.localEulerAngles.y : doorPivot.localEulerAngles.y -360;
+            angle = reversePivotDirection? Mathf.Abs(angle) : angle;
+            if (angle <= maximumPivotAngle)
+            {
+                doorPivot?.Rotate(Vector3.up, DoorSpeed * Time.deltaTime * (reversePivotDirection ? -1 : 1));
+            }
+            
+            else
+            {
+                DoorOpen = false;
+                Rigidbody rigidbody = GetComponent<Rigidbody>();
+                if (rigidbody != null && makeKinematicOnOpen)
+                {
+                    rigidbody.isKinematic = true;
+                }  
+            }
+
+            yield return null;
+
+        }
+    }
 }
